6b85c603b8e288211b0856b2bfa371ea6e29e6b1
PessahDerek <pesaderek1738@gmail.com>
2023-03-12 00:59:07 +0300

    'test'

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     
   return (
       <div className='app'>
-        <NavBar />
+        {/* <NavBar /> */}
         <Routes>
             <Route exact path='/' element={<Home/>} />
             <Route exact path='/portfolio' element={<Portfolio />} />
