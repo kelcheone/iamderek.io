616349527a152e4b6c605323d37c1ae0b0d15654
PessahDerek <pesaderek1738@gmail.com>
2023-03-12 01:10:21 +0300

    'update'

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { Route, Routes } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
+import NavBar from './components/NavBar'
 
 function App() {
     
   return (
       <div className='app'>
-        
+        <NavBar />
         <Routes>
             <Route exact path='/' element={<Home/>} />
             <Route exact path='/portfolio' element={<Portfolio />} />
